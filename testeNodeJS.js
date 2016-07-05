aa1a3b177b303b70340954b56d4307bb84b58d33
fredericoandros <fredi.rs@gmail.com>
2016-07-05 09:47:28 -0300

    Add files via upload

---
+++ testeNodeJS.js
@@ -0,0 +1,10 @@
+var http = requere('http');
+var server = http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("<h1>Ola Mundo!</h1>");
+    response.end();
+});
+
+server.listen(3000,function (){
+    console.log("NodeJS funcionando");
+});
